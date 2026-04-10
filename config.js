d17f71a5c4321e80a8958452f08af2bd585d0b11
刘天硕 <liutianshuo@liutianshuodeMacBook-Pro-1776.local>
2026-04-10 20:49:03 +0100

    Update submission endpoint config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.QUESTIONNAIRE_CONFIG = {
   projectName: "cross-speaker-questionnaire",
-  submissionEndpoint: "https://script.google.com/macros/s/AKfycbw519DGjEP-7UVkCavHo7YUocuzCn_nrOE7PydvfI757ci0gZZ2nTNntK2DwnXX2Cximw/exec"
+  submissionEndpoint: "https://script.google.com/macros/s/AKfycbywwLVk1zWcFwwUqR_JbTtCdvuJEMbSqBq3oJgWrpHbgzxlVvkXtjP2l9vbfVLWdRZNTg/exec"
 };
