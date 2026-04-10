463dceb0144b8f65a936a6c7845e8be80ac6393f
刘天硕 <liutianshuo@MacBookPro.powerhub>
2026-04-10 16:18:27 +0100

    Use deployed Apps Script endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.QUESTIONNAIRE_CONFIG = {
   projectName: "cross-speaker-questionnaire",
-  submissionEndpoint: "https://script.google.com/macros/s/AKfycbyL82LMB_-kSfKcLKAZUba1cN3PfNlknJmPveA6xBY/dev"
+  submissionEndpoint: "https://script.google.com/macros/s/AKfycbwq8onoWu7Hl5UpkKe32HSTBUv2kNHBypTrqGYJz2tmPJTyyHHlIAvwi_MmYXrQAQm2/exec"
 };
